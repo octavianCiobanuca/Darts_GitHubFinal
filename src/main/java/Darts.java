cee80b65eb487c9772490cf1599005283ecb29aa
Octavian <octavian.ciobanuca@gmail.com>
2019-01-27 13:05:03 +0200

    first commit

---
+++ src/main/java/Darts.java
@@ -0,0 +1,11 @@
+public class Darts {
+    //Darts class in Master
+    enum Multiplier {
+        DOUBLE, TRIPLE, XX;
+    }
+    private int score = 301;
+    private int dartsLeft = 3;
+    private boolean isFinished = false;
+    private int turn = 1;
+    private int lastTurnScore = score;
+}
---
+++ src/test/java/DartsTest.java
@@ -0,0 +1,12 @@
+//DartsTest ckass in Master
+import org.junit.Before;
+
+public class DartsTest {
+    private Darts dartBoard;
+
+    @Before
+    public void setUp(){
+        dartBoard = new Darts();
+    }
+
+}
