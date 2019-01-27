0682487ec743e2b5244f21e5d3e4a917609906cb
Albert <albert_is_89@yahoo.com>
2019-01-27 13:56:24 +0200

    second commit

--- src/main/java/Darts.java
+++ src/main/java/Darts.java
@@ -8,4 +8,51 @@ public class Darts {
     private boolean isFinished = false;
     private int turn = 1;
     private int lastTurnScore = score;
+
+    public void darts(int scoreTemp) {
+        updateDartBoard(scoreTemp, null);
+    }
+
+    public void darts(int i, Multiplier m) {
+        int multiplier;
+        int scoreTemp;
+        switch (m) {
+            case DOUBLE:
+                multiplier = 2;
+                break;
+            case TRIPLE:
+                multiplier = 3;
+                break;
+            default:
+                multiplier = 1;
+                break;
+        }
+        scoreTemp = multiplier * i;
+        updateDartBoard(scoreTemp, m);
+    }
+
+    private void updateDartBoard(int scoreTemp, Multiplier m){
+        if(dartsLeft==1) {
+            turn++;
+            dartsLeft = 3;
+        } else {
+            dartsLeft--;
+        }
+        int throwScore = score - scoreTemp;
+        if(throwScore == 0 && m == Multiplier.DOUBLE){
+            isFinished = true;
+            score = throwScore;
+            return;
+        }
+        if(throwScore <= 1){
+            throwScore = lastTurnScore;
+
+        }
+        if(throwScore > 1 && dartsLeft == 3){
+            lastTurnScore = throwScore;
+        }
+        score = throwScore;
+    }
+
+
 }
